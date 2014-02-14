38fcceb514745cc7681ba212b4ef6997cca5e576
Jack Zabolotnyi <jackz@jackz.in.ua>
2014-02-14 19:57:37 +0100

    Added comments

--- gearmancmd.py
+++ gearmancmd.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Command-based gearman module. """
+""" GearmanCMD: helper to process commands in Tornado-fasion. """
 
 import json
 
@@ -7,6 +7,8 @@ from threading import Event, Thread
 from multiprocessing.dummy import Pipe
 from gearman.worker import GearmanWorker
 from gearman import DataEncoder
+
+__author__ = 'Jack Zabolotnyi <mclate@mclate.com>'
 
 
 class JSONDataEncoder(DataEncoder):
@@ -28,8 +30,23 @@ class JSONGearmanWorker(GearmanWorker):
 
 
 class GearmanCMD(GearmanWorker):
-    """ Command-based gearman worker. """
+    """ Command-based gearman worker.
 
+    Runs gearman in separate thread and dispatch jobs
+    between different classes registered with `register_task`
+    method (should be called before `run`).
+
+    Shipped with JSON encoder that will try to treat all
+    messages as JSON strings and convert them into dicts.
+    This can be overridden by replacing worker with
+    different class (GearmanWorker child)
+
+    Be informed that all tasks (and responses) are passed through one Pipe,
+    meaning that all tasks are processed in one thread.
+
+    """
+
+    # Override this worker in child class to use different Gearman worker
     worker = JSONGearmanWorker
 
     _queues = {}
@@ -44,12 +61,15 @@ class GearmanCMD(GearmanWorker):
     def __init__(self, servers, command=None, **kwargs):
         """ Constructor.
 
-        Accept list of servers to connect to and command argument
-        that will be searched in passed commands to route requests to.
+        Accept list of servers to connect to and `command` argument
+        that will be searched in received task. This value will be used
+        to route tasks to different routines in client class.
 
         By default we will look for "command" key in incoming dict.
+        One can override `dispatch` method to implement different logic.
 
-        Use poll_timout to define delay between poll attempts
+        Use poll_timout to define poll interval which will
+        be used by gearman worker as well as by internal Pipe
 
         """
 
@@ -59,7 +79,7 @@ class GearmanCMD(GearmanWorker):
         self._poll_timeout = kwargs.get('poll_timeout', .1)
 
     def _create_thread(self):
-        """ Initialize worker thread. """
+        """ Initialize worker thread and communication Pipe. """
         self._pipe_in, self._pipe_out = Pipe()
         self._trigger = Event()
 
@@ -77,14 +97,19 @@ class GearmanCMD(GearmanWorker):
         self._handle.daemon = False
 
     def _thread(self, worker_class, servers, pipe, queues, trigger, timeout):
-        """ Executed in separate thread. Reads commands from gearman. """
+        """ Executed in separate thread.
+
+        Read commands from gearman and send them to Pipe for processing.
+        Wait for response (or stop trigger) from client and replies to gearman
+
+        """
 
         def _poll_event_handler(activity):
             """ Function to determine if we should stop after this poll. """
             return not trigger.is_set()
 
         def task_handler(gearman_worker, gearman_job):
-            """ Handler receive task from gearman and put it in the queue. """
+            """ Handle received task and put it in the queue. """
             pipe.send((gearman_job.task, gearman_job.data,))
 
             while not trigger.is_set() and not pipe.poll(timeout):
@@ -100,11 +125,16 @@ class GearmanCMD(GearmanWorker):
         worker.work(poll_timeout=timeout)
 
     def register_task(self, queue, target):
-        """ Register queue and target class to process this queue. """
+        """ Register queue and target class to process this queue.
+
+        Should be called before `run` in order to make
+        internal worker listen for this queue
+
+        """
         self._queues.update({queue: target})
 
     def run(self):
-        """ Start separate thread. """
+        """ Start worker thread; pass received tasks for processing. """
         self._create_thread()
         self._handle.start()
 
@@ -116,24 +146,31 @@ class GearmanCMD(GearmanWorker):
 
             try:
                 response = self._process_task(queue, task)
-            except Exception, e:
+            except Exception:
                 raise
 
+            # At this point pipe can be closed by `stop` method
             if self._pipe_out:
                 self._pipe_out.send(response)
 
     def stop(self):
-        """ Stop worker, terminate thread and finish processing tasks. """
+        """ Finish processing current task; stop worker; terminate thread. """
         self._trigger.set()
         self._pipe_in.close()
         self._pipe_out.close()
         self._handle.join()
 
     def dispatch(self, queue, task):
-        """ Method to define routine to be executed by particular task in queue
+        """ Method determine routine to be used for particular task in queue.
 
         This method could be overriden by user
         to implement complex user-defined logic.
+
+        Accept queue name and whole task for inspection.
+
+        First it will try to find `dispatch` method in queue class and call it.
+        If there is no such method, use value from command key
+        (defined in constructor, default "command")
 
         Return method name (string) or None.
         Task will be ignored if None is returned
@@ -172,7 +209,12 @@ class GearmanCMD(GearmanWorker):
 
 
 class GearmanCMDQueue(object):
-    """ Class to process commands from gearman queue. """
+    """ Basic class to process commands from gearman queue.
+
+    One would like to override it with his own class
+    and will with methods to process commands
+
+    """
 
     def default(self, gcmd, task):
         """ Default handler. """
